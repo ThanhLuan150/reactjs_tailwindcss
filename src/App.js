6ee154e334dd70c5e47b030a340c58b08d5cb13d
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-07-21 12:01:17 +0700

    luan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Content from './components/Content/Content';
 import Navbar from './components/Nar/Navbar';
 import Slogan from './components/Slogan/Slogan';
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div  className='font-sora overflow-y-auto overflow-w-hidden h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to bg-purple-700 text-white'>
             <Navbar/>
             <Slogan/>
+            <Content/>
         </div>
     </div>
     
--- src/components/Nar/Navbar.js
+++ src/components/Nar/Navbar.js
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import {BsFillCloudSunFill} from 'react-icons/bs';
+import { MdOutlineClose } from "react-icons/md";
 import { HiOutlineMenu} from 'react-icons/hi';
 const Navbar = () => {
+    const [openMenu , setOpenMenu] = useState(false);
+    const handleMenu =() =>{
+        setOpenMenu(!openMenu);
+    }
     return(
        <nav className="flex items-center ">
             <div className="flex items-center ">
@@ -8,7 +14,21 @@ const Navbar = () => {
                 <BsFillCloudSunFill size={"24px"} color="#e9c46a" className='cursor-pointer'/>
             </div>
         <ul className='ml-auto text-16 font-semibold'>
-            <HiOutlineMenu size={"24px"} className='cursor-pointer'/>
+            {openMenu ? (
+                <MdOutlineClose size={"24px"} className="cursor-pointer" onClick={handleMenu}/>
+            ):(
+                <HiOutlineMenu size={"24px"} className='cursor-pointer'onClick={handleMenu}/>
+            )}
+           {
+            openMenu && (
+                <div className='absolute right-8 bg-white p-8 text-black text-center text-13'>
+                    <li className='cursor-pointer'>Feature</li>
+                    <li className='cursor-pointer'>Menu</li>
+                    <li className='cursor-pointer'>Our Story</li>
+                    <li className='cursor-pointer'>Contact</li>
+                </div>
+            )
+           }
         </ul>
        </nav>
     )
